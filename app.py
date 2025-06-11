51bd5ade4cdd1a1099b910785480131c54027c67
HarishReddy1708 <harish@ctruh.com>
2025-06-12 00:34:14 +0530

    Update host configuration to localhost for socketio and fix indentation in questions list

--- app.py
+++ app.py
@@ -85,4 +85,4 @@ def handle_message(message):
 
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host="0.0.0.0", port=5000)
+    socketio.run(app, debug=True, host="127.0.0.1", port=5000)
